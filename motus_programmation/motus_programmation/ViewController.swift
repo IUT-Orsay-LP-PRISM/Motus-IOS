62edf03a9c77bc9f494751a352e707a64c835acc
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-05-09 11:14:27 +0200

    verification des mots

---
+++ motus_programmation/motus_programmation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  motus_programmation
+//
+//  Created by Romain Fromentin on 09/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Game.verifyWord(inputWord: "meldois")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ motus_programmation/motus_programmationUITests/motus_programmationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  motus_programmationUITestsLaunchTests.swift
+//  motus_programmationUITests
+//
+//  Created by Romain Fromentin on 09/05/2023.
+//
+
+import XCTest
+
+final class motus_programmationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
